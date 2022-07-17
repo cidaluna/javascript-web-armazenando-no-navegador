fbb4a30f286c65b979c875f47922d3c48abc711d
cidaluna <cidalunna@hotmail.com>
2022-07-17 20:28:17 -0300

    return the value of the target array at index one

--- js/main.js
+++ js/main.js
@@ -4,5 +4,6 @@ form.addEventListener('submit', (evento) => {
     evento.preventDefault(); // interromper o evento padrao
     // console.log("funcionou o clique do botao no formulario");
     // console.log(evento); // retorna o objeto inteiro, como tags, source, target e outras opções
-    console.log(evento.target[0].value)
+    console.log(evento.target[0].value); // retorna o valor do elemento no input nome
+    console.log(evento.target[1].value); // retorna o valor do elemento no input quantidade
 });

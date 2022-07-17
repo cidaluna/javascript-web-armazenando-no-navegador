2a5690a4722d74867f8c7911e95fefa56cb52b3d
cidaluna <cidalunna@hotmail.com>
2022-07-17 20:33:57 -0300

    return the element value according to the input name

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ const form = document.getElementById('novoItem');
 form.addEventListener('submit', (evento) => {
     evento.preventDefault(); // interromper o evento padrao
     // console.log("funcionou o clique do botao no formulario");
-    // console.log(evento); // retorna o objeto inteiro, como tags, source, target e outras opções
-    console.log(evento.target[0].value); // retorna o valor do elemento no input nome
-    console.log(evento.target[1].value); // retorna o valor do elemento no input quantidade
+    // console.log(evento); // retorna o objeto inteiro, como tags, source, target, lista elementos e outras opções
+    // console.log(evento.target[0].value); // retorna o valor do elemento no input nome
+    // console.log(evento.target[1].value); // retorna o valor do elemento no input quantidade
+    console.log(evento.target.elements['nome'].value);
+    console.log(evento.target.elements['quantidade'].value);
 });

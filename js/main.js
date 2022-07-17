0eab87954799a40971b7156f9db3598570666ff6
cidaluna <cidalunna@hotmail.com>
2022-07-17 20:17:18 -0300

    return object data

--- js/main.js
+++ js/main.js
@@ -4,4 +4,4 @@ form.addEventListener('submit', (evento) => {
     evento.preventDefault(); // interromper o evento padrao
     //console.log("funcionou o clique do botao no formulario");
     console.log(evento); // retorna o objeto
-})
+});

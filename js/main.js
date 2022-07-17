fae169d2ee9d1efeafe603db678c0fce9564a679
cidaluna <cidalunna@hotmail.com>
2022-07-17 20:25:48 -0300

    return the value of the target array at index zero

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const form = document.getElementById('novoItem');
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault(); // interromper o evento padrao
-    //console.log("funcionou o clique do botao no formulario");
-    console.log(evento); // retorna o objeto
+    // console.log("funcionou o clique do botao no formulario");
+    // console.log(evento); // retorna o objeto inteiro, como tags, source, target e outras opções
+    console.log(evento.target[0].value)
 });

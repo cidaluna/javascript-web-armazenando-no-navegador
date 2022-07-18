68338fc0a27532d10d2fe5e0af7e11cc7b12bb77
cidaluna <cidalunna@hotmail.com>
2022-07-18 07:18:55 -0300

    after creating item clear input field

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const lista = document.getElementById('lista');
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault(); // interromper o evento padrao
+
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['coxinha'];
+
     // console.log("funcionou o clique do botao no formulario");
     // console.log(evento); // retorna o objeto inteiro, como tags, source, target, lista elementos e outras opções
     // console.log(evento.target[0].value); // retorna o valor do elemento no input nome
@@ -11,7 +15,12 @@ form.addEventListener('submit', (evento) => {
     // console.log(evento.target.elements['coxinha'].value); // retorna o valor do elemento de acordo com o input name (quantidade)
 
     // chama funcao que cria o elemento enviando os valores por parametro
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['coxinha'].value);
+    criaElemento(nome.value, quantidade.value);
+
+    // apos criar elemento limpar o campo do input
+    nome.value = '';
+    quantidade.value = '';
+
 });
 
 function criaElemento(nome, quantidade){

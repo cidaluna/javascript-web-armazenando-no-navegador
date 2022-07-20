2c2720b760940bf632962a026e77fa585b8de16d
cidaluna <cidalunna@hotmail.com>
2022-07-20 20:13:31 -0300

    created function delete item

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-const form = document.getElementById('novoItem');
-const lista = document.getElementById('lista');
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
 // verifica se ja existe itens no localStorage, se nao existir, criar um array vazio
 // porem, para pegar os itens é preciso transformar de volta a string em objeto javascript por meio do JSON.parse
-const itens = JSON.parse(localStorage.getItem('itens')) || []; 
+const itens = JSON.parse(localStorage.getItem("itens")) || []; 
 
 // pagina é carregada buscando os elementos do localStorage e iterando e criando cada um dos elementos
 itens.forEach((elemento) => {
     criaElemento(elemento);
 });
 
-form.addEventListener('submit', (evento) => {
+form.addEventListener("submit", (evento) => {
     evento.preventDefault(); // interromper o evento padrao
     const nome = evento.target.elements['nome'];
-    const quantidade = evento.target.elements['coxinha'];
+    const quantidade = evento.target.elements['quantidade'];
 
     // verificar se o elemento ja existe, entao soma a quantidade apenas
     const existe = itens.find(elemento => elemento.nome === nome.value);
@@ -20,8 +20,8 @@ form.addEventListener('submit', (evento) => {
     // para salvar multiplos itens no localStorage
     // vamos enviar um Objeto (itemAtual), caso contrario, ele ira sobrescrever cada item enviado
     const itemAtual = {
-        'nome': nome.value,
-        'quantidade': quantidade.value
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
 
     if(existe){
@@ -29,10 +29,12 @@ form.addEventListener('submit', (evento) => {
 
         atualizaElemento(itemAtual);
 
-        itens[existe.id] = itemAtual;
+        // itens[existe.id] = itemAtual;
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
     }else{
         // cria o id com o valor do tamanho da lista itens
-        itemAtual.id = itens.length;
+        //itemAtual.id = itens.length;
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
 
         // chama funcao que cria o elemento enviando os valores por parametro
         criaElemento(itemAtual);
@@ -43,36 +45,47 @@ form.addEventListener('submit', (evento) => {
     }
 
     // transformar o objeto em uma string(pq só entende string) e passa o array itens por parametro
-    localStorage.setItem('itens', JSON.stringify(itens));  
+    localStorage.setItem("itens", JSON.stringify(itens));  
 
     // apos criar elemento limpar o campo do input
-    nome.value = '';
-    quantidade.value = '';
+    nome.value = "";
+    quantidade.value = "";
 
 });
 
 function criaElemento(item){
 
     // <li class="item"><strong>7</strong>Camisas</li>
-    const novoItem = document.createElement('li');
-    novoItem.classList.add('item');
+    const novoItem = document.createElement("li");
+    novoItem.classList.add("item");
 
-    const numeroItem = document.createElement('strong');
+    const numeroItem = document.createElement("strong");
     numeroItem.innerHTML = item.quantidade;
     numeroItem.dataset.id = item.id;
-    novoItem.innerHTML += item.nome;
     novoItem.appendChild(numeroItem); // define a hierarquia que a tag strong estará dentro da tag <li>
+    novoItem.innerHTML += item.nome;
+    novoItem.appendChild(botaoDeleta(item.id));
     lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item){
-    document.querySelector('[data-id='+item.id+']').innerHTML = item.quantidade;
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 }
 
-function botaoDeleta(){
-    const elementoBotao = document.createElement('button');
-    elementoBotao.innerHTML = X;
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerHTML = "X";
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    })
     return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
 
 /* Anotações
